fb7d12af11d9af0ced77cd33eb5427f0369faf4d
LiorGandin <85108100+LiorGandin@users.noreply.github.com>
2021-09-15 17:28:45 +0300

    Fixing models

--- Middlewares/verifySignUp.js
+++ Middlewares/verifySignUp.js
@@ -1,5 +1,5 @@
-const db = require("../models");
-const User = db.user;
+//const db = require("../models/UserModel");
+const User = require("../models/UserModel");
 
 checkDuplicateUsernameOrEmail = (req, res, next) => {
   // Username

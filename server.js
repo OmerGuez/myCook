be311eb6d740ceeca41cdba5ee7427a9c43f9517
LiorGandin <85108100+LiorGandin@users.noreply.github.com>
2021-09-17 00:07:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ dotenv.config({ path: "./config.env" });
 var corsOptions = {
   origin: "http://localhost:8100/",
 };
-
+app.use(express.static('Client'));
 app.use(cors());
 
 // parse requests of content-type - application/json

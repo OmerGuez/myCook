7cdfc22212d948ae27e0d5f2e9e7f5aff06d2310
LiorGandin <85108100+LiorGandin@users.noreply.github.com>
2021-09-17 10:17:56 +0300

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,7 @@
-const db = require("../models");
+//const db = require("../models");
 const config = require("../config/auth.config");
 
-const User = db.user;
+const User = require("../models/UserModel");
 
 var jwt = require("jsonwebtoken");
 var bcrypt = require("bcryptjs");
